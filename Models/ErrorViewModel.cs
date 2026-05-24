e7bfd02182de44b85df29b1d1a1cca7c5ee6d9a1
chirag patel <cap1990a2@outlook.com>
2026-05-24 14:03:31 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Az400WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
